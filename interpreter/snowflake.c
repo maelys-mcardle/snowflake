a97e237c8be956996927c3685a21c31074afd79e
Maëlys McArdle <airseal@hotmail.com>
2020-02-14 16:45:01 -0500

    Update snowflake.c

--- interpreter/snowflake.c
+++ interpreter/snowflake.c
@@ -88,9 +88,114 @@ bool instruction_to_string(char instruction, char *instruction_string)
     case 00:
       copy_mnemonic(instruction_string, "###");
       break;
+    case 01:
+      copy_mnemonic(instruction_string, "   ");
+      break;
+    case 02:
+      copy_mnemonic(instruction_string, " <<");
+      break;
+    case 03:
+      copy_mnemonic(instruction_string, " >>");
+      break;
+    case 04:
+      copy_mnemonic(instruction_string, "  =");
+      break;
+    case 05:
+      copy_mnemonic(instruction_string, "DEL");
+      break;
+    case 06:
+      copy_mnemonic(instruction_string, "TYP");
+      break;
+    case 10:
+      copy_mnemonic(instruction_string, "VAR");
+      break;
+    case 11:
+      copy_mnemonic(instruction_string, "BLN");
+      break;
+    case 12:
+      copy_mnemonic(instruction_string, "INT");
+      break;
+    case 13:
+      copy_mnemonic(instruction_string, "FLT");
+      break;
+    case 14:
+      copy_mnemonic(instruction_string, "STR");
+      break;
+    case 15:
+      copy_mnemonic(instruction_string, " []");
+      break;
+    case 20:
+    case 21:
+      copy_mnemonic(instruction_string, " ->");
+      break;
+    case 22:
+      copy_mnemonic(instruction_string, "IF=");
+      break;
+    case 23:
+      copy_mnemonic(instruction_string, "IF!");
+      break;
+    case 23:
+      copy_mnemonic(instruction_string, "IF>");
+      break;
+    case 24:
+      copy_mnemonic(instruction_string, "IF<");
+      break;
+    case 30:
+      copy_mnemonic(instruction_string, "  +");
+      break;
+    case 31:
+      copy_mnemonic(instruction_string, "  -");
+      break;
+    case 32:
+      copy_mnemonic(instruction_string, "  *");
+      break;
+    case 33:
+      copy_mnemonic(instruction_string, "  /");
+      break;
+    case 34:
+      copy_mnemonic(instruction_string, " **");
+      break;
+    case 40:
+      copy_mnemonic(instruction_string, "  !");
+      break;
+    case 41:
+      copy_mnemonic(instruction_string, "  &");
+      break;
+    case 42:
+      copy_mnemonic(instruction_string, "  |");
+      break;
+    case 43:
+      copy_mnemonic(instruction_string, "  ^");
+      break;
+    case 50:
+      copy_mnemonic(instruction_string, "+[]");
+      break;
+    case 51:
+      copy_mnemonic(instruction_string, "[]+");
+      break;
+    case 52:
+      copy_mnemonic(instruction_string, "?[]");
+      break;
+    case 53:
+      copy_mnemonic(instruction_string, "[]?");
+      break;
+    case 54:
+      copy_mnemonic(instruction_string, "-[]");
+      break;
+    case 55:
+      copy_mnemonic(instruction_string, "[]-");
+      break;
+    case 56:
+      copy_mnemonic(instruction_string, "[>]");
+      break;
+    case 57:
+      copy_mnemonic(instruction_string, "[<]");
+      break;
+    case 58:
+      copy_mnemonic(instruction_string, "[#]");
+      break;
     default:
       // No instruction exists.
-      copy_mnemonic(instruction_string, "   ");
       return false;
   }
   
