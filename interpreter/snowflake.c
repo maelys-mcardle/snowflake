3feff1ccaf2f426b6596567fae077336fbf08f50
Maëlys McArdle <airseal@hotmail.com>
2020-02-13 21:01:32 -0500

    Added interpreter.

---
+++ interpreter/snowflake.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "snowflake.h"
+
+/* Starting point of the program. */
+int main () 
+{
+  // Header.
+  printf("SNOWFLAKE INTERPRETER\n"
+        "======================\n");
+
+  // Run the interpeter loop.
+  run_interpreter_loop();
+
+  // Exit without error.
+  return(0);
+}
+
+/* The main REPL loop. */
+void run_interpreter_loop()
+{
+  printf("> ");
+  char instruction = get_instruction();
+}
+
+/* Returns the instruction code. */
+char get_instruction()
+{
+  // When reading instruction:
+  //  * Everything before the subsequent space or newline is part of the instruction
+  //  * Instructions are made up of one or two digits
+  char instruction[2];
+  char input_character;
+  instruction[0] = getchar();
+  instruction[1] = getchar();
+}
+
+void instruction_mnemonic(char instruction, char *mnemonic)
+{
+  switch (instruction) {
+    case 00:
+      mnemonic = "###";
+      break;
+    default:
+      mnemonic = "";
+  }
+}
---
+++ interpreter/snowflake.h
@@ -0,0 +1,3 @@
+void run_interpreter_loop();
+char get_instruction();
+void instruction_mnemonic(char instruction, char *mnemonic);
