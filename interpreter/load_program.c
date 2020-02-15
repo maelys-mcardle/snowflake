8f41bf5e795c0a92359972f97d8f9ac1675c8927
Maëlys McArdle <airseal@hotmail.com>
2020-02-15 13:49:40 -0500

    parsing field.

--- interpreter/load_program.c
+++ interpreter/load_program.c
@@ -2,7 +2,9 @@
 #include "load_program.h"
 #include "structures.h"
 
-/* Loads a line of text into the program. */
+/* Loads a line of text into the program.
+ * @return true if a line contained an instruction, false if not.
+ */
 bool load_line_into_program(SnowflakeProgram *program, char *line, int max_line_length)
 {
     // Programs are of the format:
@@ -28,6 +30,10 @@ bool load_line_into_program(SnowflakeProgram *program, char *line, int max_line_
     // Remove comments from the line.
     discard_comment(line, max_line_length);
 
+    // Load the instruction.
+    char instruction;
+    int cursor = parse_instruction(line, max_line_length, &instruction);
+
     printf("%s", line);
 
     return true;
@@ -35,32 +41,131 @@ bool load_line_into_program(SnowflakeProgram *program, char *line, int max_line_
 
 /* Discards the comment on the line. 
  * @return true if comment was removed, false otherwise.
-*/
+ */
 bool discard_comment(char *line, int max_line_length)
 {
     bool last_char_is_semicolon = false;
+
     // Remove anything that follows the ";;"
     for (int index = 0; index < max_line_length; index++) {
 
-        // Second ";" found.
-        if (line[index] == COMMENT_SYMBOL &&
-            last_char_is_semicolon) {
+        char character = line[index];
 
-            // End the line here.
-            line[index-1] = '\0';
+        // Stop at newline or break.
+        if (is_string_end(character))
+        {
+            return false;
+        }
+
+        // Second ";" found.
+        else if (character == CHAR_COMMENT &&
+            last_char_is_semicolon)
+        {
+
+            // Set end the line here.
+            line[index-1] = CHAR_END_STRING;
             return true;
         }
 
         // First ";" found.
-        else if (line[index] == COMMENT_SYMBOL) {
+        else if (character == CHAR_COMMENT)
+        {
             last_char_is_semicolon = true;
         }
 
         // Any other character found.
-        else {
+        else
+        {
             last_char_is_semicolon = false;
         }
     }
 
     return false;
+}
+
+/* Extracts the instruction from the line.
+ * @return the last character with the position.
+ */
+int parse_instruction(char *line, int max_line_length, char *instruction)
+{
+    int index = 0;
+    char max_instruction_size = 3;
+    char instruction_string[max_instruction_size];
+    char instruction_index = 0;
+
+    int end = parse_field(line, max_line_length, true, 
+        0, instruction_string, max_instruction_size);
+
+    return end;
+}
+
+/* Extracts the instruction from the line.
+ * @return the last character with the position.
+ */
+int parse_field(char *line, int max_line_length, bool stop_at_whitespace, 
+    int start, char *output, int max_output_size)
+{
+    int index = start;
+    char output_index = 0;
+
+    // Remove anything that follows the ";;"
+    for (index; index < max_line_length; index++) {
+        char character = line[index];
+
+        // Stop at newline or break.
+        if (is_string_end(character) ||
+            output_index == max_output_size - 1)
+        {
+            output[output_index] = CHAR_END_STRING;
+            return index;
+        }
+
+        // Ignore whitespace before instruction.
+        if (output_index == 0 &&
+            is_whitespace(character))
+        {
+            // do nothing.
+        }
+
+        // Whitespace after instruction is understood
+        // as the end of the instruction.
+        else if (stop_at_whitespace && 
+            output_index > 0 &&
+            is_whitespace(character))
+        {
+            // End it here.
+            output[output_index] = CHAR_END_STRING;
+            return index;
+        }
+
+        // Load text in between.
+        else {
+            output[output_index] = character;
+            output_index++;
+        }
+    }        
+
+    return index;
+}
+
+bool is_whitespace(char character)
+{
+    switch (character) {
+        case CHAR_SPACE:
+        case CHAR_TAB:
+            return true;
+        default:
+            return false;
+    }
+}
+
+bool is_string_end(char character)
+{
+       switch (character) {
+        case CHAR_NEWLINE:
+        case CHAR_END_STRING:
+            return true;
+        default:
+            return false;
+    } 
 }
--- interpreter/load_program.h
+++ interpreter/load_program.h
@@ -3,9 +3,18 @@
 
 #include <stdbool.h>
 #include "structures.h"
-#define COMMENT_SYMBOL ';'
+#define CHAR_COMMENT ';'
+#define CHAR_NEWLINE '\n'
+#define CHAR_SPACE ' '
+#define CHAR_TAB '\t'
+#define CHAR_END_STRING '\0'
 
 bool load_line_into_program(SnowflakeProgram *program, char *line, int max_line_length);
 bool discard_comment(char *line, int max_line_length);
+int parse_instruction(char *line, int max_line_length, char *instruction);
+int parse_field(char *line, int max_line_length, bool stop_at_whitespace, 
+    int start, char *output, int max_output_size);
+bool is_whitespace(char character);
+bool is_string_end(char character);
 
 #endif
