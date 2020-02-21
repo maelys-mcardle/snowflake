0075e4bbffc6d6f28276d395c2a089c4ad569922
Maëlys McArdle <airseal@hotmail.com>
2020-02-21 11:12:03 -0500

    Update program.h

--- interpreter/headers/program.h
+++ interpreter/headers/program.h
@@ -16,7 +16,7 @@ Program *new_program();
 void free_program(Program *program);
 bool append_instruction_to_program(Program *program, Instruction *instruction);
 void print_snowflake_program(Program *program);
-void print_parameter(ParameterType type, ParameterValue value);
-void print_device(int device);
+bool print_parameter(ParameterType type, ParameterValue value);
+bool print_device(int device);
 
 #endif
