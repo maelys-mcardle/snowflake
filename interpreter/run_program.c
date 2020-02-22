a8b7fd1afb08d411e67117969a367d8935ee6576
Maëlys McArdle <airseal@hotmail.com>
2020-02-21 22:46:48 -0500

    Fleshed out executing more.

--- interpreter/run_program.c
+++ interpreter/run_program.c
@@ -21,6 +21,58 @@ void run_program(Program *program)
 
 void run_instruction(Program *program, Instruction *instruction, int *instruction_pointer)
 {
-    *instruction_pointer += 1;
+    log_debug("Executing %c%c%c at %02i\n", 
+        instruction->info.mnemonic[0],
+        instruction->info.mnemonic[1],
+        instruction->info.mnemonic[2],
+        *instruction_pointer);
+
+    switch (instruction->instruction)
+    {
+        case INSTRUCTION_COMMENT:
+        case INSTRUCTION_LABEL:
+        case INSTRUCTION_NAME_BANK:
+            // no-op.
+            *instruction_pointer += 1;
+            break;
+        case INSTRUCTION_OUTPUT:
+        case INSTRUCTION_INPUT:
+        case INSTRUCTION_COPY:
+        case INSTRUCTION_TYPE:
+        case INSTRUCTION_VARIABLE:
+        case INSTRUCTION_BOOLEAN:
+        case INSTRUCTION_INTEGER:
+        case INSTRUCTION_FLOAT:
+        case INSTRUCTION_STRING:
+        case INSTRUCTION_ARRAY:
+        case INSTRUCTION_JUMP_LABEL:
+        case INSTRUCTION_JUMP_BANK:
+        case INSTRUCTION_IF_EQUAL:
+        case INSTRUCTION_IF_NOT_EQUAL:
+        case INSTRUCTION_IF_GREATER_THAN:
+        case INSTRUCTION_IF_LESSER_THAN:
+        case INSTRUCTION_ADD:
+        case INSTRUCTION_SUBTRACT:
+        case INSTRUCTION_MULTIPLY:
+        case INSTRUCTION_DIVIDE:
+        case INSTRUCTION_POWER:
+        case INSTRUCTION_NOT:
+        case INSTRUCTION_AND:
+        case INSTRUCTION_OR:
+        case INSTRUCTION_XOR:
+        case INSTRUCTION_ARRAY_SIZE:
+        case INSTRUCTION_PREPEND_ARRAY:
+        case INSTRUCTION_APPEND_ARRAY:
+        case INSTRUCTION_GET_FIRST:
+        case INSTRUCTION_GET_LAST:
+        case INSTRUCTION_REMOVE_FIRST:
+        case INSTRUCTION_REMOVE_LAST:
+        case INSTRUCTION_MOVE_TO_INDEX:
+        case INSTRUCTION_MOVE_TO_FIRST:
+        default:
+            *instruction_pointer += 1;
+            break;
+    }
     return;
 }
+
