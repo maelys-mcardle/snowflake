8455cccbe0b57f1d7ab1025153130711d2d7b66b
Maëlys McArdle <airseal@hotmail.com>
2020-02-21 19:11:35 -0500

    Printing program fixed.

--- interpreter/headers/print_program.h
+++ interpreter/headers/print_program.h
@@ -6,10 +6,9 @@
 
 void print_program(Program *program);
 void print_instruction(Program *program, Instruction *instruction);
-bool print_parameter(Program *program, ParameterType type, ParameterValue value);
-void print_label(Program *program, int label);
-void print_bank(Program *program, int bank);
+bool print_parameter(Program *program, int current_instruction, ParameterType type, ParameterValue value);
+void print_identifier(Program *program, int current_instruction, int naming_instruction, int target_identifier, char *format);
 bool print_device(int device);
-char *get_name(Program *program, int target_instruction, int target_identifier);
+char *get_name(Program *program, int naming_instruction, int target_identifier);
 
 #endif
--- interpreter/print_program.c
+++ interpreter/print_program.c
@@ -29,6 +29,7 @@ void print_instruction(Program *program, Instruction *instruction)
     // Print first parameter.
     bool has_parameter = print_parameter(
         program,
+        instruction->instruction,
         instruction->info.parameters.first, 
         instruction->parameters.first);
 
@@ -41,6 +42,7 @@ void print_instruction(Program *program, Instruction *instruction)
     // Print second parameter.
     print_parameter(
         program,
+        instruction->instruction,
         instruction->info.parameters.second, 
         instruction->parameters.second);
 
@@ -51,7 +53,7 @@ void print_instruction(Program *program, Instruction *instruction)
 /* Prints a parameter, if one is defined.
  * @return True if a parameter is defined; false if not.
  */
-bool print_parameter(Program *program, ParameterType type, ParameterValue value)
+bool print_parameter(Program *program, int current_instruction, ParameterType type, ParameterValue value)
 {
     ParameterType type_without_flags = type & PARAMETER_WITHOUT_FLAGS;
     switch (type_without_flags)
@@ -59,10 +61,10 @@ bool print_parameter(Program *program, ParameterType type, ParameterValue value)
         case PARAMETER_NONE:
             return false;
         case PARAMETER_BANK:
-            print_bank(program, value.integer);
+            print_identifier(program, current_instruction, INSTRUCTION_NAME_BANK, value.integer, "@%02i");
             return true;
         case PARAMETER_LABEL:
-            print_label(program, value.integer);
+            print_identifier(program, current_instruction, INSTRUCTION_LABEL, value.integer, ":%02i");
             return true;
         case PARAMETER_DEVICE:
             return print_device(value.integer);
@@ -78,40 +80,33 @@ bool print_parameter(Program *program, ParameterType type, ParameterValue value)
     }
 }
 
-void print_label(Program *program, int label)
+void print_identifier(Program *program, int current_instruction, int naming_instruction, int target_identifier, char *format)
 {
+    bool found_name = false;
+
     // Lookup name of label.
-    char *label_name = get_name(program, INSTRUCTION_LABEL, label);
-    if (label_name != NULL)
+    if (current_instruction != naming_instruction)
     {
-        print("%s", label_name);
+        char *identifier_name = get_name(program, naming_instruction, target_identifier);
+        if (identifier_name != NULL)
+        {
+            print("%s", identifier_name);
+            found_name = true;
+        }
     }
-    else
+
+    if (!found_name)
     {
-        print(":%02i", label);
+        print(format, target_identifier);
     }
 }
 
-void print_bank(Program *program, int bank)
-{
-    // Lookup name of bank.
-    char *bank_name = get_name(program, INSTRUCTION_NAME_BANK, bank);
-    if (bank_name != NULL)
-    {
-        print("%s", bank_name);
-    }
-    else
-    {
-        print("@%02i", bank);
-    }
-}
-
-char *get_name(Program *program, int target_instruction, int target_identifier)
+char *get_name(Program *program, int naming_instruction, int target_identifier)
 {
     for (int i = 0; i < program->instructions.count; i++)
     {
         Instruction *instruction = program->instructions.instructions[i];
-        if (instruction->instruction == target_instruction &&
+        if (instruction->instruction == naming_instruction &&
             instruction->parameters.first.integer == target_identifier)
         {
             return instruction->parameters.second.string;
