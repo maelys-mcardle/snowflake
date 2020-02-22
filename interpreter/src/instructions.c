d4c82ea96ecdf4a6c463536c14cfcdcad44cd9bc
Maëlys McArdle <airseal@hotmail.com>
2020-02-22 10:58:15 -0500

    Converted indents.

--- interpreter/src/instructions.c
+++ interpreter/src/instructions.c
@@ -35,277 +35,277 @@ void free_parameter(ParameterType type, ParameterValue value)
 }
 
 InstructionInfo get_instruction_info(short instruction, bool *exists)
-{  
-  InstructionInfo info;
+{
+    InstructionInfo info;
 
-  switch (instruction) {
-    case INSTRUCTION_COMMENT:
-      info.mnemonic[0] = '!';
-      info.mnemonic[1] = '!';
-      info.mnemonic[2] = '!';
-      info.parameters.first = PARAMETER_LITERAL | PARAMETER_OPTIONAL;
-      info.parameters.second = PARAMETER_NONE;
-      break;
-    case INSTRUCTION_LABEL:
-      info.mnemonic[0] = '#';
-      info.mnemonic[1] = '#';
-      info.mnemonic[2] = '#';
-      info.parameters.first = PARAMETER_LABEL;
-      info.parameters.second = PARAMETER_LITERAL | PARAMETER_OPTIONAL;
-      break;
-    case INSTRUCTION_NAME_BANK:
-      info.mnemonic[0] = ' ';
-      info.mnemonic[1] = ' ';
-      info.mnemonic[2] = ' ';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_LITERAL;
-      break;
-    case INSTRUCTION_OUTPUT:
-      info.mnemonic[0] = ' ';
-      info.mnemonic[1] = '<';
-      info.mnemonic[2] = '<';
-      info.parameters.first = PARAMETER_DEVICE;
-      info.parameters.second = PARAMETER_BANK;
-      break;
-    case INSTRUCTION_INPUT:
-      info.mnemonic[0] = ' ';
-      info.mnemonic[1] = '>';
-      info.mnemonic[2] = '>';
-      info.parameters.first = PARAMETER_DEVICE;
-      info.parameters.second = PARAMETER_BANK;
-      break;
-    case INSTRUCTION_COPY:
-      info.mnemonic[0] = 'D';
-      info.mnemonic[1] = 'E';
-      info.mnemonic[2] = 'L';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_NONE;
-      break;
-    case INSTRUCTION_TYPE:
-      info.mnemonic[0] = 'T';
-      info.mnemonic[1] = 'Y';
-      info.mnemonic[2] = 'P';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_BANK;
-      break;
-    case INSTRUCTION_VARIABLE:
-      info.mnemonic[0] = 'V';
-      info.mnemonic[1] = 'A';
-      info.mnemonic[2] = 'R';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_LITERAL;
-      break;
-    case INSTRUCTION_BOOLEAN:
-      info.mnemonic[0] = 'B';
-      info.mnemonic[1] = 'L';
-      info.mnemonic[2] = 'N';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_LITERAL;
-      break;
-    case INSTRUCTION_INTEGER:
-      info.mnemonic[0] = 'I';
-      info.mnemonic[1] = 'N';
-      info.mnemonic[2] = 'T';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_LITERAL;
-      break;
-    case INSTRUCTION_FLOAT:
-      info.mnemonic[0] = 'F';
-      info.mnemonic[1] = 'L';
-      info.mnemonic[2] = 'T';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_LITERAL;
-      break;
-    case INSTRUCTION_STRING:
-      info.mnemonic[0] = 'S';
-      info.mnemonic[1] = 'T';
-      info.mnemonic[2] = 'R';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_LITERAL;
-      break;
-    case INSTRUCTION_ARRAY:
-      info.mnemonic[0] = ' ';
-      info.mnemonic[1] = '[';
-      info.mnemonic[2] = ']';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_NONE;
-      break;
-    case INSTRUCTION_JUMP_LABEL:
-      info.mnemonic[0] = ' ';
-      info.mnemonic[1] = '-';
-      info.mnemonic[2] = '>';
-      info.parameters.first = PARAMETER_LABEL;
-      info.parameters.second = PARAMETER_NONE;
-      break;
-    case INSTRUCTION_JUMP_BANK:
-      info.mnemonic[0] = ' ';
-      info.mnemonic[1] = '-';
-      info.mnemonic[2] = '>';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_NONE;
-      break;
-    case INSTRUCTION_IF_EQUAL:
-      info.mnemonic[0] = 'I';
-      info.mnemonic[1] = 'F';
-      info.mnemonic[2] = '=';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_BANK;
-      break;
-    case INSTRUCTION_IF_NOT_EQUAL:
-      info.mnemonic[0] = 'I';
-      info.mnemonic[1] = 'F';
-      info.mnemonic[2] = '!';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_BANK;
-      break;
-    case INSTRUCTION_IF_GREATER_THAN:
-      info.mnemonic[0] = 'I';
-      info.mnemonic[1] = 'F';
-      info.mnemonic[2] = '>';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_BANK;
-      break;
-    case INSTRUCTION_IF_LESSER_THAN:
-      info.mnemonic[0] = 'I';
-      info.mnemonic[1] = 'F';
-      info.mnemonic[2] = '<';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_BANK;
-      break;
-    case INSTRUCTION_ADD:
-      info.mnemonic[0] = ' ';
-      info.mnemonic[1] = ' ';
-      info.mnemonic[2] = '+';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_BANK;
-      break;
-    case INSTRUCTION_SUBTRACT:
-      info.mnemonic[0] = ' ';
-      info.mnemonic[1] = ' ';
-      info.mnemonic[2] = '-';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_BANK;
-      break;
-    case INSTRUCTION_MULTIPLY:
-      info.mnemonic[0] = ' ';
-      info.mnemonic[1] = ' ';
-      info.mnemonic[2] = '*';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_BANK;
-      break;
-    case INSTRUCTION_DIVIDE:
-      info.mnemonic[0] = ' ';
-      info.mnemonic[1] = ' ';
-      info.mnemonic[2] = '/';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_BANK;
-      break;
-    case INSTRUCTION_POWER:
-      info.mnemonic[0] = ' ';
-      info.mnemonic[1] = '*';
-      info.mnemonic[2] = '*';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_BANK;
-      break;
-    case INSTRUCTION_NOT:
-      info.mnemonic[0] = ' ';
-      info.mnemonic[1] = ' ';
-      info.mnemonic[2] = '!';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_NONE;
-      break;
-    case INSTRUCTION_AND:
-      info.mnemonic[0] = ' ';
-      info.mnemonic[1] = ' ';
-      info.mnemonic[2] = '&';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_BANK;
-      break;
-    case INSTRUCTION_OR:
-      info.mnemonic[0] = ' ';
-      info.mnemonic[1] = ' ';
-      info.mnemonic[2] = '|';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_BANK;
-      break;
-    case INSTRUCTION_XOR:
-      info.mnemonic[0] = ' ';
-      info.mnemonic[1] = ' ';
-      info.mnemonic[2] = '^';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_BANK;
-      break;
-    case INSTRUCTION_ARRAY_SIZE:
-      info.mnemonic[0] = '[';
-      info.mnemonic[1] = '#';
-      info.mnemonic[2] = ']';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_BANK;
-      break;
-    case INSTRUCTION_PREPEND_ARRAY:
-      info.mnemonic[0] = '+';
-      info.mnemonic[1] = '[';
-      info.mnemonic[2] = ']';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_BANK;
-      break;
-    case INSTRUCTION_APPEND_ARRAY:
-      info.mnemonic[0] = '[';
-      info.mnemonic[1] = ']';
-      info.mnemonic[2] = '+';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_BANK;
-      break;
-    case INSTRUCTION_GET_FIRST:
-      info.mnemonic[0] = '?';
-      info.mnemonic[1] = '[';
-      info.mnemonic[2] = ']';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_BANK;
-      break;
-    case INSTRUCTION_GET_LAST:
-      info.mnemonic[0] = '[';
-      info.mnemonic[1] = ']';
-      info.mnemonic[2] = '?';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_BANK;
-      break;
-    case INSTRUCTION_REMOVE_FIRST:
-      info.mnemonic[0] = '-';
-      info.mnemonic[1] = '[';
-      info.mnemonic[2] = ']';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_BANK;
-      break;
-    case INSTRUCTION_REMOVE_LAST:
-      info.mnemonic[0] = '[';
-      info.mnemonic[1] = ']';
-      info.mnemonic[2] = '-';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_BANK;
-      break;
-    case INSTRUCTION_MOVE_TO_INDEX:
-      info.mnemonic[0] = '[';
-      info.mnemonic[1] = '>';
-      info.mnemonic[2] = ']';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_BANK;
-      break;
-    case INSTRUCTION_MOVE_TO_FIRST:
-      info.mnemonic[0] = '[';
-      info.mnemonic[1] = '<';
-      info.mnemonic[2] = ']';
-      info.parameters.first = PARAMETER_BANK;
-      info.parameters.second = PARAMETER_BANK;
-      break;
-    default:
-      // No instruction exists.
-      *exists = false;
-      return info;
-  }
-  
-  // Instruction exists.
-  *exists = true;
-  return info;
+    switch (instruction) {
+        case INSTRUCTION_COMMENT:
+            info.mnemonic[0] = '!';
+            info.mnemonic[1] = '!';
+            info.mnemonic[2] = '!';
+            info.parameters.first = PARAMETER_LITERAL | PARAMETER_OPTIONAL;
+            info.parameters.second = PARAMETER_NONE;
+            break;
+        case INSTRUCTION_LABEL:
+            info.mnemonic[0] = '#';
+            info.mnemonic[1] = '#';
+            info.mnemonic[2] = '#';
+            info.parameters.first = PARAMETER_LABEL;
+            info.parameters.second = PARAMETER_LITERAL | PARAMETER_OPTIONAL;
+            break;
+        case INSTRUCTION_NAME_BANK:
+            info.mnemonic[0] = ' ';
+            info.mnemonic[1] = ' ';
+            info.mnemonic[2] = ' ';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_LITERAL;
+            break;
+        case INSTRUCTION_OUTPUT:
+            info.mnemonic[0] = ' ';
+            info.mnemonic[1] = '<';
+            info.mnemonic[2] = '<';
+            info.parameters.first = PARAMETER_DEVICE;
+            info.parameters.second = PARAMETER_BANK;
+            break;
+        case INSTRUCTION_INPUT:
+            info.mnemonic[0] = ' ';
+            info.mnemonic[1] = '>';
+            info.mnemonic[2] = '>';
+            info.parameters.first = PARAMETER_DEVICE;
+            info.parameters.second = PARAMETER_BANK;
+            break;
+        case INSTRUCTION_COPY:
+            info.mnemonic[0] = 'D';
+            info.mnemonic[1] = 'E';
+            info.mnemonic[2] = 'L';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_NONE;
+            break;
+        case INSTRUCTION_TYPE:
+            info.mnemonic[0] = 'T';
+            info.mnemonic[1] = 'Y';
+            info.mnemonic[2] = 'P';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_BANK;
+            break;
+        case INSTRUCTION_VARIABLE:
+            info.mnemonic[0] = 'V';
+            info.mnemonic[1] = 'A';
+            info.mnemonic[2] = 'R';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_LITERAL;
+            break;
+        case INSTRUCTION_BOOLEAN:
+            info.mnemonic[0] = 'B';
+            info.mnemonic[1] = 'L';
+            info.mnemonic[2] = 'N';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_LITERAL;
+            break;
+        case INSTRUCTION_INTEGER:
+            info.mnemonic[0] = 'I';
+            info.mnemonic[1] = 'N';
+            info.mnemonic[2] = 'T';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_LITERAL;
+            break;
+        case INSTRUCTION_FLOAT:
+            info.mnemonic[0] = 'F';
+            info.mnemonic[1] = 'L';
+            info.mnemonic[2] = 'T';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_LITERAL;
+            break;
+        case INSTRUCTION_STRING:
+            info.mnemonic[0] = 'S';
+            info.mnemonic[1] = 'T';
+            info.mnemonic[2] = 'R';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_LITERAL;
+            break;
+        case INSTRUCTION_ARRAY:
+            info.mnemonic[0] = ' ';
+            info.mnemonic[1] = '[';
+            info.mnemonic[2] = ']';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_NONE;
+            break;
+        case INSTRUCTION_JUMP_LABEL:
+            info.mnemonic[0] = ' ';
+            info.mnemonic[1] = '-';
+            info.mnemonic[2] = '>';
+            info.parameters.first = PARAMETER_LABEL;
+            info.parameters.second = PARAMETER_NONE;
+            break;
+        case INSTRUCTION_JUMP_BANK:
+            info.mnemonic[0] = ' ';
+            info.mnemonic[1] = '-';
+            info.mnemonic[2] = '>';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_NONE;
+            break;
+        case INSTRUCTION_IF_EQUAL:
+            info.mnemonic[0] = 'I';
+            info.mnemonic[1] = 'F';
+            info.mnemonic[2] = '=';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_BANK;
+            break;
+        case INSTRUCTION_IF_NOT_EQUAL:
+            info.mnemonic[0] = 'I';
+            info.mnemonic[1] = 'F';
+            info.mnemonic[2] = '!';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_BANK;
+            break;
+        case INSTRUCTION_IF_GREATER_THAN:
+            info.mnemonic[0] = 'I';
+            info.mnemonic[1] = 'F';
+            info.mnemonic[2] = '>';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_BANK;
+            break;
+        case INSTRUCTION_IF_LESSER_THAN:
+            info.mnemonic[0] = 'I';
+            info.mnemonic[1] = 'F';
+            info.mnemonic[2] = '<';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_BANK;
+            break;
+        case INSTRUCTION_ADD:
+            info.mnemonic[0] = ' ';
+            info.mnemonic[1] = ' ';
+            info.mnemonic[2] = '+';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_BANK;
+            break;
+        case INSTRUCTION_SUBTRACT:
+            info.mnemonic[0] = ' ';
+            info.mnemonic[1] = ' ';
+            info.mnemonic[2] = '-';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_BANK;
+            break;
+        case INSTRUCTION_MULTIPLY:
+            info.mnemonic[0] = ' ';
+            info.mnemonic[1] = ' ';
+            info.mnemonic[2] = '*';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_BANK;
+            break;
+        case INSTRUCTION_DIVIDE:
+            info.mnemonic[0] = ' ';
+            info.mnemonic[1] = ' ';
+            info.mnemonic[2] = '/';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_BANK;
+            break;
+        case INSTRUCTION_POWER:
+            info.mnemonic[0] = ' ';
+            info.mnemonic[1] = '*';
+            info.mnemonic[2] = '*';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_BANK;
+            break;
+        case INSTRUCTION_NOT:
+            info.mnemonic[0] = ' ';
+            info.mnemonic[1] = ' ';
+            info.mnemonic[2] = '!';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_NONE;
+            break;
+        case INSTRUCTION_AND:
+            info.mnemonic[0] = ' ';
+            info.mnemonic[1] = ' ';
+            info.mnemonic[2] = '&';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_BANK;
+            break;
+        case INSTRUCTION_OR:
+            info.mnemonic[0] = ' ';
+            info.mnemonic[1] = ' ';
+            info.mnemonic[2] = '|';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_BANK;
+            break;
+        case INSTRUCTION_XOR:
+            info.mnemonic[0] = ' ';
+            info.mnemonic[1] = ' ';
+            info.mnemonic[2] = '^';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_BANK;
+            break;
+        case INSTRUCTION_ARRAY_SIZE:
+            info.mnemonic[0] = '[';
+            info.mnemonic[1] = '#';
+            info.mnemonic[2] = ']';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_BANK;
+            break;
+        case INSTRUCTION_PREPEND_ARRAY:
+            info.mnemonic[0] = '+';
+            info.mnemonic[1] = '[';
+            info.mnemonic[2] = ']';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_BANK;
+            break;
+        case INSTRUCTION_APPEND_ARRAY:
+            info.mnemonic[0] = '[';
+            info.mnemonic[1] = ']';
+            info.mnemonic[2] = '+';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_BANK;
+            break;
+        case INSTRUCTION_GET_FIRST:
+            info.mnemonic[0] = '?';
+            info.mnemonic[1] = '[';
+            info.mnemonic[2] = ']';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_BANK;
+            break;
+        case INSTRUCTION_GET_LAST:
+            info.mnemonic[0] = '[';
+            info.mnemonic[1] = ']';
+            info.mnemonic[2] = '?';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_BANK;
+            break;
+        case INSTRUCTION_REMOVE_FIRST:
+            info.mnemonic[0] = '-';
+            info.mnemonic[1] = '[';
+            info.mnemonic[2] = ']';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_BANK;
+            break;
+        case INSTRUCTION_REMOVE_LAST:
+            info.mnemonic[0] = '[';
+            info.mnemonic[1] = ']';
+            info.mnemonic[2] = '-';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_BANK;
+            break;
+        case INSTRUCTION_MOVE_TO_INDEX:
+            info.mnemonic[0] = '[';
+            info.mnemonic[1] = '>';
+            info.mnemonic[2] = ']';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_BANK;
+            break;
+        case INSTRUCTION_MOVE_TO_FIRST:
+            info.mnemonic[0] = '[';
+            info.mnemonic[1] = '<';
+            info.mnemonic[2] = ']';
+            info.parameters.first = PARAMETER_BANK;
+            info.parameters.second = PARAMETER_BANK;
+            break;
+        default:
+            // No instruction exists.
+            *exists = false;
+            return info;
+    }
+    
+    // Instruction exists.
+    *exists = true;
+    return info;
 }
 
--- interpreter/src/main.c
+++ interpreter/src/main.c
@@ -14,110 +14,110 @@
 /* Starting point of the program. */
 int main (int argc, char **argv) 
 {
-  bool no_unrecognized_arguments = false;
-  bool print_code = false;
-  bool run_code = false;
-  char *file_path = NULL;
+    bool no_unrecognized_arguments = false;
+    bool print_code = false;
+    bool run_code = false;
+    char *file_path = NULL;
 
-  // Parse command-line arguments.
-  no_unrecognized_arguments = parse_arguments(argc, argv, &print_code, &run_code, &file_path);
+    // Parse command-line arguments.
+    no_unrecognized_arguments = parse_arguments(argc, argv, &print_code, &run_code, &file_path);
 
-  // Abort if there was an unrecognized argument.
-  if (!no_unrecognized_arguments)
-  {
-    log_error(ERROR_MESG_UNRECOGNIZED_ARGUMENT);
-    return ERROR_CODE_UNRECOGNIZED_ARGUMENT;
-  }
+    // Abort if there was an unrecognized argument.
+    if (!no_unrecognized_arguments)
+    {
+        log_error(ERROR_MESG_UNRECOGNIZED_ARGUMENT);
+        return ERROR_CODE_UNRECOGNIZED_ARGUMENT;
+    }
 
-  // Abort if no action was selected.
-  if (!print_code && !run_code)
-  {
-    log_error(ERROR_MESG_NO_ACTION_SELECTED);
-    return ERROR_CODE_NO_ACTION_SELECTED;
-  }
+    // Abort if no action was selected.
+    if (!print_code && !run_code)
+    {
+        log_error(ERROR_MESG_NO_ACTION_SELECTED);
+        return ERROR_CODE_NO_ACTION_SELECTED;
+    }
 
-  // Parse the snowflake file and do the action(s).
-  return parse_snowflake_file_and_run(print_code, run_code, file_path);
+    // Parse the snowflake file and do the action(s).
+    return parse_snowflake_file_and_run(print_code, run_code, file_path);
 }
 
 int parse_snowflake_file_and_run(bool print_code, bool run_code, char *snowflake_file)
 {
-  // Initialize the program.
-  Program *program = new_program();
+    // Initialize the program.
+    Program *program = new_program();
  
-  // Load the snowflake code.
-  int return_code = parse_snowflake_file(program, snowflake_file);
-  
-  // Print out the code.
-  if (print_code)
-  {
-    print_program(program);
-  }
+    // Load the snowflake code.
+    int return_code = parse_snowflake_file(program, snowflake_file);
+    
+    // Print out the code.
+    if (print_code)
+    {
+        print_program(program);
+    }
 
-  // Execute the code.
-  if (run_code)
-  {
-    run_program(program);
-  }
-  
-  // Free the memory allocated to the program.
-  free_program(program);
+    // Execute the code.
+    if (run_code)
+    {
+        run_program(program);
+    }
+    
+    // Free the memory allocated to the program.
+    free_program(program);
 
-  // Exit with the return code supplied by processing
-  // the snowflake file.
-  return return_code;
+    // Exit with the return code supplied by processing
+    // the snowflake file.
+    return return_code;
 }
 
 bool parse_arguments(int argc, char **argv, bool *print_code, bool *run_code, char **file_path)
 {
-  static struct option long_options[] =
-  {
-    {"file", required_argument, 0, 'f'},
-    {"debug", no_argument, 0, 'd'},
-    {"show-code", no_argument, 0, 's'},
-    {"run", no_argument, 0, 'r'},
-    {0, 0, 0, 0}
-  };
-
-  while (1)
-  {
-    int option_index = 0;
-
-    int current_option = 
-      getopt_long (argc, argv, "f:dsr",
-      long_options, &option_index);
-
-    // End of options.
-    if (current_option == -1)
+    static struct option long_options[] =
     {
-      break;
+        {"file", required_argument, 0, 'f'},
+        {"debug", no_argument, 0, 'd'},
+        {"show-code", no_argument, 0, 's'},
+        {"run", no_argument, 0, 'r'},
+        {0, 0, 0, 0}
+    };
+
+    while (1)
+    {
+        int option_index = 0;
+
+        int current_option = 
+            getopt_long (argc, argv, "f:dsr",
+            long_options, &option_index);
+
+        // End of options.
+        if (current_option == -1)
+        {
+            break;
+        }
+
+        switch (current_option)
+        {
+            case 'd':
+                enable_debug_mode();
+                break;
+            
+            case 's':
+                *print_code = true;
+                break;
+            
+            case 'f':
+                *file_path = optarg;
+                break;
+
+            case 'r':
+                *run_code = true;
+                break;
+
+            case '?':
+                // Unknown argument.
+                return false;
+        }        
+
     }
 
-    switch (current_option)
-    {
-      case 'd':
-        enable_debug_mode();
-        break;
-      
-      case 's':
-        *print_code = true;
-        break;
-      
-      case 'f':
-        *file_path = optarg;
-        break;
-
-      case 'r':
-        *run_code = true;
-        break;
-
-      case '?':
-        // Unknown argument.
-        return false;
-    }    
-
-  }
-
-  // No unknown argument.
-  return true;
+    // No unknown argument.
+    return true;
 }
