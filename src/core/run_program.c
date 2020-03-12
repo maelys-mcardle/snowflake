c5c30e675120344a74bc76f428b5b2b64a8eab7c
Maëlys McArdle <airseal@hotmail.com>
2020-03-11 20:49:24 -0400

    Simplified run program.

--- src/core/run_program.c
+++ src/core/run_program.c
@@ -50,101 +50,62 @@ bool run_instruction(Program *program, Instruction *instruction, int *instructio
         }
     }
 
+    // All instructions and their associated functions.
+    InstructionFunction all_functions[] = {
+        [INSTRUCTION_COMMENT]       = &instruction_noop,
+        [INSTRUCTION_LABEL]         = &instruction_noop,
+        [INSTRUCTION_NAME_BANK]     = &instruction_noop,
+        [INSTRUCTION_OUTPUT]        = &instruction_output,
+        [INSTRUCTION_INPUT]         = &instruction_input,
+        [INSTRUCTION_COPY]          = &instruction_copy,
+        [INSTRUCTION_CONVERT]       = &instruction_convert,
+        [INSTRUCTION_TYPE]          = &instruction_type,
+        [INSTRUCTION_DELETE]        = &instruction_delete,
+        [INSTRUCTION_LENGTH]        = &instruction_length,
+        [INSTRUCTION_VARIABLE]      = &instruction_variable,
+        [INSTRUCTION_BOOLEAN]       = &instruction_boolean,
+        [INSTRUCTION_INTEGER]       = &instruction_integer,
+        [INSTRUCTION_FLOAT]         = &instruction_float,
+        [INSTRUCTION_STRING]        = &instruction_string,
+        [INSTRUCTION_ARRAY]         = &instruction_array,
+        [INSTRUCTION_JUMP_LABEL]    = &instruction_noop,
+        [INSTRUCTION_JUMP_BANK]     = &instruction_noop,
+        [INSTRUCTION_IF_EQUAL]      = &instruction_noop,
+        [INSTRUCTION_IF_NOT_EQUAL]  = &instruction_noop,
+        [INSTRUCTION_IF_GREATER_THAN] = &instruction_noop,
+        [INSTRUCTION_IF_LESSER_THAN]  = &instruction_noop,
+        [INSTRUCTION_ADD]           = &instruction_add,
+        [INSTRUCTION_SUBTRACT]      = &instruction_subtract,
+        [INSTRUCTION_MULTIPLY]      = &instruction_multiply,
+        [INSTRUCTION_DIVIDE]        = &instruction_divide,
+        [INSTRUCTION_MODULO]        = &instruction_modulo,
+        [INSTRUCTION_POWER]         = &instruction_power,
+        [INSTRUCTION_SQUARE_ROOT]   = &instruction_square_root,
+        [INSTRUCTION_NOT]           = &instruction_noop,
+        [INSTRUCTION_AND]           = &instruction_noop,
+        [INSTRUCTION_OR]            = &instruction_noop,
+        [INSTRUCTION_XOR]           = &instruction_noop,
+        [INSTRUCTION_PREPEND_ARRAY] = &instruction_noop,
+        [INSTRUCTION_APPEND_ARRAY]  = &instruction_noop,
+        [INSTRUCTION_GET_FIRST]     = &instruction_noop,
+        [INSTRUCTION_GET_LAST]      = &instruction_noop,
+        [INSTRUCTION_REMOVE_FIRST]  = &instruction_noop,
+        [INSTRUCTION_REMOVE_LAST]   = &instruction_noop,
+        [INSTRUCTION_MOVE_FIRST_TO_INDEX] = &instruction_noop,
+        [INSTRUCTION_MOVE_LAST_TO_INDEX]  = &instruction_noop,
+        [INSTRUCTION_MOVE_TO_FIRST]       = &instruction_noop,
+        [INSTRUCTION_MOVE_TO_LAST]        = &instruction_noop,
+    };
+
     // Get the instruction to execute.
+    InstructionCode first_instruction = INSTRUCTION_COMMENT;
+    InstructionCode last_instruction = INSTRUCTION_MOVE_TO_LAST;
+    InstructionCode instruction_code = instruction->instruction;
     InstructionFunction execute_intruction = NULL;
-    switch (instruction->instruction)
+    if (instruction_code >= first_instruction && 
+        instruction_code <= last_instruction)
     {
-        case INSTRUCTION_COMMENT:
-        case INSTRUCTION_LABEL:
-        case INSTRUCTION_NAME_BANK:
-            execute_intruction = &instruction_noop;
-            break;
-        case INSTRUCTION_OUTPUT:
-            execute_intruction = &instruction_output;
-            break;
-        case INSTRUCTION_INPUT:
-            execute_intruction = &instruction_input;
-            break;
-        case INSTRUCTION_COPY:
-            execute_intruction = &instruction_copy;
-            break;
-        case INSTRUCTION_CONVERT:
-            execute_intruction = &instruction_convert;
-            break;
-        case INSTRUCTION_TYPE:
-            execute_intruction = &instruction_type;
-            break;
-        case INSTRUCTION_DELETE:
-            execute_intruction = &instruction_delete;
-            break;
-        case INSTRUCTION_LENGTH:
-            execute_intruction = &instruction_length;
-            break;
-        case INSTRUCTION_VARIABLE:
-            execute_intruction = &instruction_variable;
-            break;
-        case INSTRUCTION_BOOLEAN:
-            execute_intruction = &instruction_boolean;
-            break;
-        case INSTRUCTION_INTEGER:
-            execute_intruction = &instruction_integer;
-            break;
-        case INSTRUCTION_FLOAT:
-            execute_intruction = &instruction_float;
-            break;
-        case INSTRUCTION_STRING:
-            execute_intruction = &instruction_string;
-            break;
-        case INSTRUCTION_ARRAY:
-            execute_intruction = &instruction_array;
-            break;
-        case INSTRUCTION_JUMP_LABEL:
-        case INSTRUCTION_JUMP_BANK:
-        case INSTRUCTION_IF_EQUAL:
-        case INSTRUCTION_IF_NOT_EQUAL:
-        case INSTRUCTION_IF_GREATER_THAN:
-        case INSTRUCTION_IF_LESSER_THAN:
-            *instruction_pointer += 1;
-            break;
-        case INSTRUCTION_ADD:
-            execute_intruction = &instruction_add;
-            break;
-        case INSTRUCTION_SUBTRACT:
-            execute_intruction = &instruction_subtract;
-            break;
-        case INSTRUCTION_MULTIPLY:
-            execute_intruction = &instruction_multiply;
-            break;
-        case INSTRUCTION_DIVIDE:
-            execute_intruction = &instruction_divide;
-            break;
-        case INSTRUCTION_MODULO:
-            execute_intruction = &instruction_modulo;
-            break;
-        case INSTRUCTION_POWER:
-            execute_intruction = &instruction_power;
-            break;
-        case INSTRUCTION_SQUARE_ROOT:
-            execute_intruction = &instruction_square_root;
-            break;
-        case INSTRUCTION_NOT:
-        case INSTRUCTION_AND:
-        case INSTRUCTION_OR:
-        case INSTRUCTION_XOR:
-        case INSTRUCTION_PREPEND_ARRAY:
-        case INSTRUCTION_APPEND_ARRAY:
-        case INSTRUCTION_GET_FIRST:
-        case INSTRUCTION_GET_LAST:
-        case INSTRUCTION_REMOVE_FIRST:
-        case INSTRUCTION_REMOVE_LAST:
-        case INSTRUCTION_MOVE_FIRST_TO_INDEX:
-        case INSTRUCTION_MOVE_LAST_TO_INDEX:
-        case INSTRUCTION_MOVE_TO_FIRST:
-        case INSTRUCTION_MOVE_TO_LAST:
-            *instruction_pointer += 1;
-            break;
-        default:
-            break;
+        execute_intruction = all_functions[instruction_code];
     }
 
     // Execute the command.
