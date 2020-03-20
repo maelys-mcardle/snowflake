17f8b4733464472daaede65405eccaa7b515b394
Maëlys McArdle <airseal@hotmail.com>
2020-03-20 19:48:30 -0400

    Array code work.

--- src/core/array.c
+++ src/core/array.c
@@ -29,13 +29,14 @@ void free_array(Array *array)
         for (ArrayIndex index = 0; index < array->count; index++)
         {
             log_debug("Free array item %i of %i.\n", index, array->count);
-            array->free_array_item_function(array->items[index]);
+            ArrayItem *item = array->items[index];
+            array->free_array_item_function(item);
         }
         free(array);
     }
 }
 
-void free_array_item(ArrayItem item)
+void free_array_item(ArrayItem *item)
 {
     log_debug("Call to free item at %p.\n", item);
 }
@@ -47,38 +48,49 @@ Array *copy_array(Array *source)
     {
         destination->copy_array_item_function = source->copy_array_item_function;
         destination->free_array_item_function = source->free_array_item_function;
-        destination->items = copy_array_items(source);
-        destination->count = (destination->items == NULL) ? 0 : source->count;
+        copy_array_items(destination, source);
     }
     return destination;
 }
 
-ArrayItem *copy_array_items(Array *source)
+bool resize_array_index(Array *array, ArrayCount new_count)
+{
+    ArrayItem **new_items = realloc(array->items, sizeof(ArrayItem *) * new_count);
+    if (new_items != NULL)
+    {
+        array->items = new_items;
+        array->count = new_count;
+        return true;
+    }
+    else
+    {
+        log_error(ERROR_MESG_COULD_NOT_ALLOCATE_MEMORY);
+        return false;
+    }
+}
+
+bool copy_array_items(Array *destination, Array *source)
 {
     if (source->count == 0)
     {
         return NULL;
     }
 
-    ArrayItem *items = malloc(sizeof(ArrayItem) * source->count);
-    if (items != NULL)
+    bool ok = resize_array_index(destination, source->count);
+    if (ok)
     {
         for (ArrayIndex index = 0; index < source->count; index++)
         {
-            items[index] = 
+            destination->items[index] = 
                 source->copy_array_item_function(
                     source->items[index]);
         }
     }
-    else
-    {
-        log_error(ERROR_MESG_COULD_NOT_ALLOCATE_MEMORY);
-    }
     
-    return items;
+    return ok;
 }
 
-ArrayItem copy_array_item(ArrayItem source_item)
+ArrayItem *copy_array_item(ArrayItem *source_item)
 {
     log_debug("Call to copy item at %p.\n", source_item);
     return source_item;
--- src/core/headers/array.h
+++ src/core/headers/array.h
@@ -4,24 +4,25 @@
 
 typedef unsigned int ArrayIndex;
 typedef ArrayIndex ArrayCount;
-typedef void *ArrayItem;
-typedef void (*FreeArrayItemFunction)(ArrayItem array_item);
-typedef ArrayItem (*CopyArrayItemFunction)(ArrayItem source_item);
+typedef void ArrayItem;
+typedef void (*FreeArrayItemFunction)(ArrayItem *array_item);
+typedef ArrayItem *(*CopyArrayItemFunction)(ArrayItem *source_item);
 
 typedef struct {
     ArrayCount count;
-    ArrayItem *items;
+    ArrayItem **items;
     FreeArrayItemFunction free_array_item_function;
     CopyArrayItemFunction copy_array_item_function;
 } Array;
 
 Array *new_array();
 void free_array(Array *array);
-void free_array_item(ArrayItem item);
+void free_array_item(ArrayItem *item);
+bool resize_array_index(Array *array, ArrayCount new_count);
 ArrayCount get_array_count(Array *array);
 Array *copy_array(Array *source);
-ArrayItem *copy_array_items(Array *source);
-ArrayItem copy_array_item(ArrayItem source_item);
+bool copy_array_items(Array *destination, Array *source);
+ArrayItem *copy_array_item(ArrayItem *source_item);
 
 bool push(Array *array, ArrayItem *item);
 bool pop(Array *array, ArrayItem *item);
