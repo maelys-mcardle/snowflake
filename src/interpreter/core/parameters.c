8941c961cbc06e92cb540939e7dc905416ef62b9
Maëlys McArdle <airseal@hotmail.com>
2020-04-05 10:56:04 -0400

    Documented parameters.c

--- src/interpreter/core/parameters.c
+++ src/interpreter/core/parameters.c
@@ -5,6 +5,12 @@
 #include "platforms/logging.h"
 #include "errors.h"
 
+/**
+ * Frees an instruction parameter.
+ * 
+ * @param value instruction parameter value.
+ * @param type instruction parameter type.
+ */
 void free_parameter(ParameterValue *value, ParameterType type)
 {
     if (is_parameter_literal(type) &&
@@ -15,6 +21,14 @@ void free_parameter(ParameterValue *value, ParameterType type)
     }
 }
 
+/**
+ * Stores a value in the instruction parameter.
+ * 
+ * @param parameter_value instruction parameter value.
+ * @param parameter_type instruction parameter type.
+ * @param parameter_string string to store in the instruction parameter value.
+ * @return whether the operation was successful.
+ */
 bool store_parameter(ParameterValue *parameter_value, ParameterType parameter_type, char *parameter_string)
 {
     // Store the parameter.
@@ -47,41 +61,89 @@ bool store_parameter(ParameterValue *parameter_value, ParameterType parameter_ty
     return true;
 }
 
+/**
+ * Whether this parameter is a literal.
+ * 
+ * @param type the type of the parameter.
+ * @return whether the parameter is a literal.
+ */
 bool is_parameter_literal(ParameterType type)
 {
     return get_parameter_type_without_flags(type) == PARAMETER_LITERAL;
 }
 
+/**
+ * Whether this parameter is a bank.
+ * 
+ * @param type the type of the parameter.
+ * @return whether the parameter is a bank.
+ */
 bool is_parameter_bank(ParameterType type)
 {
     return get_parameter_type_without_flags(type) == PARAMETER_BANK;
 }
 
+/**
+ * Whether this parameter is a label.
+ * 
+ * @param type the type of the parameter.
+ * @return whether the parameter is a label.
+ */
 bool is_parameter_label(ParameterType type)
 {
     return get_parameter_type_without_flags(type) == PARAMETER_LABEL;
 }
 
+/**
+ * Whether this parameter is a device.
+ * 
+ * @param type the type of the parameter.
+ * @return whether the parameter is a device.
+ */
 bool is_parameter_device(ParameterType type)
 {
     return get_parameter_type_without_flags(type) == PARAMETER_DEVICE;
 }
 
+/**
+ * Whether this parameter is a type type.
+ * 
+ * @param type the type of the parameter.
+ * @return whether the parameter is a type type.
+ */
 bool is_parameter_type(ParameterType type)
 {
     return get_parameter_type_without_flags(type) == PARAMETER_TYPE;
 }
 
+/**
+ * Strips any flags OR'd with the type.
+ * 
+ * @param type the type of the parameter.
+ * @return the type of the parameter stripped of any flags.
+ */
 ParameterType get_parameter_type_without_flags(ParameterType type)
 {
     return (type & PARAMETER_WITHOUT_FLAGS);
 }
 
+/**
+ * Whether this parameter type has the OPTIONAL flag.
+ * 
+ * @param type the type of the parameter.
+ * @return whether the parameter has the OPTIONAL flag.
+ */
 bool is_parameter_optional(ParameterType type)
 {
     return ((type & PARAMETER_OPTIONAL) == PARAMETER_OPTIONAL);
 }
 
+/**
+ * Whether this parameter is supposed to have no value.
+ * 
+ * @param type the type of the parameter.
+ * @return whether this parameter is supposed to have no value.
+ */
 bool is_parameter_none(ParameterType type)
 {
     return (type == PARAMETER_NONE);
