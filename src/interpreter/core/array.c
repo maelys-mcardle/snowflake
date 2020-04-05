892dea06ce096749b13c93e7a19e6d1bc271fec6
Maëlys McArdle <airseal@hotmail.com>
2020-04-05 09:58:47 -0400

    Documented array.c

--- src/interpreter/core/array.c
+++ src/interpreter/core/array.c
@@ -1,8 +1,13 @@
 #include <stdlib.h>
-#include "core/array.h"
-#include "errors.h"
 #include "platforms/logging.h"
+#include "errors.h"
+#include "core/array.h"
 
+/**
+ * Creates a new array.
+ * 
+ * @return allocated array.
+ */
 Array *new_array()
 {
     log_debug("Creating new array.\n");
@@ -21,6 +26,11 @@ Array *new_array()
     return array;
 }
 
+/**
+ * Frees an allocated array.
+ * 
+ * @param array array to free.
+ */
 void free_array(Array *array)
 {
     if (array != NULL)
@@ -36,11 +46,23 @@ void free_array(Array *array)
     }
 }
 
+/**
+ * Default implementation of the function to free an array item.
+ * This is a no-op.
+ * 
+ * @param item array item to free.
+ */
 void free_array_item(ArrayItem *item)
 {
     (void)(item);
 }
 
+/**
+ * Copies an array.
+ * 
+ * @param source original array to copy.
+ * @return copy of array.
+ */
 Array *copy_array(Array *source)
 {
     Array *destination = new_array();
@@ -53,6 +75,13 @@ Array *copy_array(Array *source)
     return destination;
 }
 
+/**
+ * Resizes the array so it can fit the new size.
+ * 
+ * @param array array to resize.
+ * @param new_count new size of array.
+ * @return whether the operation was successful.
+ */
 bool resize_array(Array *array, ArrayCount new_count)
 {
     log_debug("Resizing array to %i items.\n", new_count);
@@ -91,6 +120,13 @@ bool resize_array(Array *array, ArrayCount new_count)
     }
 }
 
+/**
+ * Copies all the items of one array into another.
+ * 
+ * @param destination where to copy the items to.
+ * @param source where to copy the items from.
+ * @return whether the operation was successful.
+ */
 bool copy_array_items(Array *destination, Array *source)
 {
     if (source == NULL || destination == NULL)
@@ -121,52 +157,110 @@ bool copy_array_items(Array *destination, Array *source)
     return ok;
 }
 
+/**
+ * Default implementation of the function to copy an array item.
+ * This is a no-op.
+ * 
+ * @param item array item to copy.
+ * @return copy of the array item.
+ */
 ArrayItem *copy_array_item(ArrayItem *source_item)
 {
     return source_item;
 }
 
+/**
+ * Get the size of the array.
+ * 
+ * @param array array to get the size of.
+ * @return size of the array.
+ */
 ArrayCount get_array_count(Array *array)
 {
     return (array != NULL) ? array->count : 0;
 }
 
+/**
+ * Prepend an item to the array.
+ * 
+ * @param array array to modify.
+ * @param item item to prepend.
+ * @return whether the operation was successful.
+ */
 bool prepend_array(Array *array, ArrayItem *item)
 {
     log_debug("Inserting item at start of array.\n");
     return insert_array_item(array, item, 0);
 }
 
+/**
+ * Append an item to the array.
+ * 
+ * @param array array to modify.
+ * @param item item to append.
+ * @return whether the operation was successful.
+ */
 bool append_array(Array *array, ArrayItem *item)
 {
     log_debug("Inserting item at end of array.\n");
     return insert_array_item(array, item, array->count);
 }
 
+/**
+ * Removes the first item of an array.
+ * 
+ * @param array array to get the item from.
+ * @return the first item of the array.
+ */
 ArrayItem *remove_array_first(Array *array)
 {
     log_debug("Removing item at start of array.\n");
     return remove_array_item(array, 0);
 }
 
+/**
+ * Removes the last item of an array.
+ * 
+ * @param array array to get the item from.
+ * @return the last item of the array.
+ */
 ArrayItem *remove_array_last(Array *array)
 {
     log_debug("Removing item at end of array.\n");
     return remove_array_item(array, array->count - 1);
 }
 
+/**
+ * Fetch the first item of an array.
+ * 
+ * @param array array to get the item from.
+ * @return the first item of the array.
+ */
 ArrayItem *get_array_first(Array *array)
 {
     log_debug("Get item at start of array.\n");
     return get_array_item(array, 0);
 }
 
+/**
+ * Fetch the last item of an array.
+ * 
+ * @param array array to get the item from.
+ * @return the last item of the array.
+ */
 ArrayItem *get_array_last(Array *array)
 {
     log_debug("Get item at end of array.\n");
     return get_array_item(array, array->count - 1);
 }
 
+/**
+ * Fetch the specified item of an array.
+ * 
+ * @param array array to get the item from.
+ * @param index_to_get index of the item in the array.
+ * @return the item from the array.
+ */
 ArrayItem *get_array_item(Array *array, ArrayIndex index_to_get)
 {
     if (index_to_get < array->count)
@@ -179,7 +273,14 @@ ArrayItem *get_array_item(Array *array, ArrayIndex index_to_get)
     }
 }
 
-/* Add an element to the index of an array. */
+/**
+ * Insert the specified item into an array.
+ * 
+ * @param array array to insert the item into.
+ * @param item the item to insert in the array.
+ * @param index_to_insert the index to insert the item at.
+ * @return whether the operation was successful.
+ */
 bool insert_array_item(Array *array, ArrayItem *item, ArrayIndex index_to_insert)
 {
     log_debug("Inserting item at position %i of array.\n", index_to_insert);
@@ -215,7 +316,13 @@ bool insert_array_item(Array *array, ArrayItem *item, ArrayIndex index_to_insert
     return true;
 }
 
-/* Remove element at the index of an array. */
+/**
+ * Remove the specified item of an array.
+ * 
+ * @param array array to remove the item from.=
+ * @param index_to_remove the index to remove the item from.
+ * @return the item that was removed.
+ */
 ArrayItem *remove_array_item(Array *array, ArrayIndex index_to_remove)
 {
     log_debug("Removing item at position %i of array.\n", index_to_remove);
