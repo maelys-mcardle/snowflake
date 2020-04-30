f7afc81d224e1209cefe1b2d1306d0579485d7fd
Maëlys McArdle <airseal@hotmail.com>
2020-04-30 19:29:19 -0400

    Device documentation.

--- src/include/core/devices.h
+++ src/include/core/devices.h
@@ -2,11 +2,15 @@
 #define SNOWFLAKE_DEVICES_H
 #include <stdbool.h>
 
+// Mnemonics for the input/output devices.
 #define DEVICE_OUT_STRING "OUT"
 #define DEVICE_IN_STRING  " IN"
 #define DEVICE_BTN_STRING "BTN"
 #define DEVICE_RND_STRING "RND"
 
+/**
+ * Enum for the intput/output devices.
+ */
 typedef enum {
     DEVICE_OUT = 0,
     DEVICE_IN  = 1,
